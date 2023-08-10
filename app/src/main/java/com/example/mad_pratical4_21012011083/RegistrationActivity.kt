42e52ab14b7881295f4f2e68b550899e4dfabb7d
student <jp6832746@gmail.com>
2023-08-10 08:53:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_pratical4_21012011083/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_pratical4_21012011083
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mad_Pratical4_21012011083"
+include(":app")
